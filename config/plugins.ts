03e5b2585cce8333287b8c787e3d24a0ee1e9b7f
pos1234 <paulosegebeyehu5@gmail.com>
2025-04-13 23:27:40 +0300

    Worked on tag and category

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,10 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+  "users-permissions": {
+    config: {
+      jwtSecret: env("JWT_SECRET", "your-default-secret-here"), // <- fallback optional
+      register: {
+        allowedFields: ["accountType"],
+      },
+    },
+  },
+});
